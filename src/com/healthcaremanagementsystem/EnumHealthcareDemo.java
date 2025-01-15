b1002d85b1ac31cdfed461a257d1715926da7e46
Sunil <sunildivase07@gmail.com>
2025-01-15 13:54:43 +0530

    initial import

--- src/com/healthcaremanagementsystem/EnumHealthcareDemo.java
+++ src/com/healthcaremanagementsystem/EnumHealthcareDemo.java
@@ -1,6 +1,7 @@
 package com.healthcaremanagementsystem;
 
 import com.healthcaremanagementsystem.model.Person;
+import com.healthcaremanagementsystem.service.DoctorService;
 import com.healthcaremanagementsystem.service.PersonService;
 
 import java.util.HashMap;
@@ -19,6 +20,12 @@ public class EnumHealthcareDemo {
         do{
             System.out.println("--------------healthcare-management-system--------------------------");
             System.out.println("1. create person");
+            System.out.println("2. create doctor");
+            System.out.println("3. create hospital");
+            System.out.println("4. create department");
+            System.out.println("5. create appointment");
+            System.out.println("6. create prescription");
+            System.out.println("7. create billing");
 
             System.out.println("please enter the option");
             option = Integer.parseInt(scanner.nextLine());
@@ -32,6 +39,9 @@ public class EnumHealthcareDemo {
                     break;
 
                 case 2:
+                    DoctorService doctorService = new DoctorService();
+                    doctorService.createDoctor();
+                    doctorService.displayDoctor();
                     System.out.println("doctor created successfully");
                     break;
 
--- src/com/healthcaremanagementsystem/service/DoctorService.java
+++ src/com/healthcaremanagementsystem/service/DoctorService.java
@@ -1,4 +1,68 @@
 package com.healthcaremanagementsystem.service;
 
+import com.healthcaremanagementsystem.User;
+import com.healthcaremanagementsystem.model.Doctor;
+
+import javax.print.Doc;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 public class DoctorService {
+
+   private static Scanner scanner = new Scanner(System.in);
+
+   private static HashMap<String, Doctor> doctors = new HashMap<>();
+
+   void printDoctor(Doctor doctor){
+       System.out.println(doctor);
+   }
+
+   public Doctor createDoctor(){
+
+       System.out.println("please enter id");
+       int doctorId = Integer.parseInt(scanner.nextLine());
+
+       System.out.println("please enter first name ");
+       String firstName = scanner.nextLine();
+
+       System.out.println("please enter last name");
+       String lastName = scanner.nextLine();
+
+       System.out.println("please enter age ");
+       int age = Integer.parseInt(scanner.nextLine());
+
+       System.out.println("please enter gender");
+       String gender = scanner.nextLine();
+
+       System.out.println("please enter contact number");
+       String contactNo = scanner.nextLine();
+
+       System.out.println("please enter speciality");
+       String speciality = scanner.nextLine().toUpperCase();
+
+       System.out.println("please enter experience");
+       int experience = Integer.parseInt(scanner.nextLine());
+
+       Doctor doctor = new Doctor();
+       doctor.setDoctorId(doctorId);
+       doctor.setFirstName(firstName);
+       doctor.setLastName(lastName);
+       doctor.setAge(age);
+       doctor.setGender(gender);
+       doctor.setContactNo(contactNo);
+       doctor.setSpeciality(User.DOCTOR);
+       doctor.setExperience(experience);
+
+
+       doctors.put(String.valueOf(1),doctor);
+       return doctor;
+   }
+   public void displayDoctor(){
+       Set<Map.Entry<String ,Doctor>> entrySet = doctors.entrySet();
+       for(Map.Entry<String,Doctor> entry : entrySet){
+           System.out.println("key: "+entry.getKey()+" "+"value: "+entry.getValue());
+       }
+   }
 }
